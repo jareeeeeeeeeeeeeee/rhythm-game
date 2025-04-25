da66f1dcb3c8e7c43b7f0026449d7b5d071f7013
Bugga314 <harrisonnewu314@gmail.com>
2025-04-25 11:45:55 -0500

    notes done?
    still need to fix lanes

--- node_2d.gd
+++ node_2d.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-var timer = 60
+var timer = 20
 
 var note_preload = preload("Note.tscn")
 
 func _process(_delta: float):
 	timer -= 1
 	if timer == 0:
-		timer = 60
+		timer = 20
 		var note = note_preload.instantiate()
 		
 		add_child(note)
--- note.gd
+++ note.gd
@@ -1,10 +1,11 @@
-extends RigidBody2D
+extends Sprite2D
 
 @export_range(1,100)
 var speed = 10
 
 func _init():
 	self.position.x = 415 + randi_range(0,7) * 150
+	self.position.y = -30
 
 func _process(delta: float):
 	self.position.y += speed
