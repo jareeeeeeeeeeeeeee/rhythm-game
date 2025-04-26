9717944de766fdabb362547562f0d597ada852f2
Bugga314 <harrisonnewu314@gmail.com>
2025-04-25 19:24:07 -0500

    notes are hittable and print times

--- note.gd
+++ note.gd
@@ -2,10 +2,14 @@ extends Sprite2D
 
 @export_range(1,100)
 var speed = 10
+var lane = randi_range(0,7)
 
 func _init():
-	self.position.x = 415 + randi_range(0,7) * 150
+	self.position.x = 415 + lane * 150
 	self.position.y = -30
+	self.add_to_group(str(lane))
 
 func _process(delta: float):
 	self.position.y += speed
+	if self.position.y >= 1100:
+		queue_free()
