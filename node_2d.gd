e167eaa34811c879c66777b726c1db23f37f28a1
Bugga314 <harrisonnewu314@gmail.com>
2025-04-26 19:26:32 -0500

    Missed notes now change the value

--- node_2d.gd
+++ node_2d.gd
@@ -12,35 +12,35 @@ func _process(_delta: float):
 		add_child(note)
 
 func _input(ev):
-	if Input.is_key_pressed(KEY_A) and not ev.echo and get_tree().get_nodes_in_group("0"):
+	if Input.is_action_pressed("button1") and not ev.echo and get_tree().get_nodes_in_group("0"):
 		var hit_note = get_tree().get_nodes_in_group("0")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
-	if Input.is_key_pressed(KEY_S) and not ev.echo and get_tree().get_nodes_in_group("1"):
+	if Input.is_action_pressed("button2") and not ev.echo and get_tree().get_nodes_in_group("1"):
 		var hit_note = get_tree().get_nodes_in_group("1")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
-	if Input.is_key_pressed(KEY_D) and not ev.echo and get_tree().get_nodes_in_group("2"):
+	if Input.is_action_pressed("button3") and not ev.echo and get_tree().get_nodes_in_group("2"):
 		var hit_note = get_tree().get_nodes_in_group("2")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
-	if Input.is_key_pressed(KEY_F) and not ev.echo and get_tree().get_nodes_in_group("3"):
+	if Input.is_action_pressed("button4") and not ev.echo and get_tree().get_nodes_in_group("3"):
 		var hit_note = get_tree().get_nodes_in_group("3")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
-	if Input.is_key_pressed(KEY_H) and not ev.echo and get_tree().get_nodes_in_group("4"):
+	if Input.is_action_pressed("button5") and not ev.echo and get_tree().get_nodes_in_group("4"):
 		var hit_note = get_tree().get_nodes_in_group("4")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
-	if Input.is_key_pressed(KEY_J) and not ev.echo and get_tree().get_nodes_in_group("5"):
+	if Input.is_action_pressed("button6") and not ev.echo and get_tree().get_nodes_in_group("5"):
 		var hit_note = get_tree().get_nodes_in_group("5")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
-	if Input.is_key_pressed(KEY_K) and not ev.echo and get_tree().get_nodes_in_group("6"):
+	if Input.is_action_pressed("button7") and not ev.echo and get_tree().get_nodes_in_group("6"):
 		var hit_note = get_tree().get_nodes_in_group("6")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
-	if Input.is_key_pressed(KEY_L) and not ev.echo and get_tree().get_nodes_in_group("7"):
+	if Input.is_action_pressed("button8") and not ev.echo and get_tree().get_nodes_in_group("7"):
 		var hit_note = get_tree().get_nodes_in_group("7")[0]
 		print(hit_note.position.y)
 		hit_note.queue_free()
