151b2cbb4c70c8c20fd72ca01f2e29cacb870220
Bugga314 <harrisonnewu314@gmail.com>
2025-05-14 09:41:07 -0500

    Update lifebar.gd

--- lifebar.gd
+++ lifebar.gd
@@ -1,5 +1,10 @@
 extends Sprite2D
 
 func _process(delta: float):
-	self.scale.x = (G.life)/160
-	self.position.x = 1480 + (self.scale.x*self.texture.get_width())/2
+	if G.life:
+		self.scale.x = (G.life)/160
+		self.position.x = 1480 + (self.scale.x*self.texture.get_width())/2
+	else:
+		self.scale.x = 6.25
+		self.position.x = 1680
+		self.modulate = Color(1,0,0)
